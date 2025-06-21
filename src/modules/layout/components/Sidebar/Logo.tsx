32bb8510a0db56bdb8f72371cdd928bb1806d032
David <102764748+davidbutsch@users.noreply.github.com>
2025-06-20 22:38:57 -0500

    navigate to root on logo click

--- src/modules/layout/components/Sidebar/Logo.tsx
+++ src/modules/layout/components/Sidebar/Logo.tsx
@@ -1,14 +1,25 @@
 import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const Logo = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate("/");
+
   return (
     <Box
       component="img"
       src="jot.svg"
-      height={40}
-      width="fit-content"
-      pl={2} // aligns logo image with action and navigation buttons
-      mb={2}
+      onClick={handleClick}
+      sx={{
+        pl: 2, // aligns logo image with action and navigation buttons
+        mb: 2,
+
+        height: 40,
+        width: "fit-content",
+
+        cursor: "pointer",
+      }}
     />
   );
 };
